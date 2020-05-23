489d02dbffe5727fe93dc40f3ce2f5e64c488a51
Ivan <63844098+n1cejkee91@users.noreply.github.com>
2020-05-23 15:46:45 +0300

    update

---
+++ Hangman/task/hangman/hangman.py
@@ -0,0 +1,9 @@
+# Write your code here
+print("H A N G M A N")
+word = 'python'
+guess = input("Guess the word: ")
+
+if guess == word:
+    print("You survived!")
+else:
+    print("You are hanged!")
